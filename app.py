07fed5baae88d9366a59b49fe8f75290ed95d652
SJana <sourabh.jana@prolifics.com>
2020-10-23 11:18:36 +0530

    update app

--- app.py
+++ app.py
@@ -24,14 +24,17 @@ app = Flask(__name__)
 def predict():
     if classifier:
         try:
-            json_ = request.json
-            print(json_)
-            query = pd.get_dummies(pd.DataFrame(json_))
-            query = query.reindex(columns=model_columns, fill_value=0)
-            print(query)
-            prediction = classifier.predict(query)
+            data = request.get_json(force=True)
+            data.update((x, [y]) for x, y in data.items())
+            data_df = pd.DataFrame.from_dict(data)
+            #json_ = request.json
+            #print(json_)
+            #query = pd.get_dummies(pd.DataFrame(json_))
+            #query = query.reindex(columns=model_columns, fill_value=0)
+            print(data_df)
+            prediction = classifier.predict(data_df)
             
-            confidence = classifier.predict_proba(query)
+            confidence = classifier.predict_proba(data_df)
             return jsonify({'prediction': str(prediction[0]), 'confidence': [(round((confidence[0][0]*100),3)),(round((confidence[0][1]*100),3))]})
            
         except:
@@ -46,7 +49,8 @@ if __name__ == '__main__':
     print ('Model loaded')
     model_columns = joblib.load("model_columns.pkl") # Load "model_columns.pkl"
     print ('Model columns loaded')
-    app.run(host='127.0.0.1', use_reloader=False,port=8080, debug=True)
+    app.run(port = 5000, debug=True)
+    #app.run(host='127.0.0.1', use_reloader=False,port=8080, debug=True)
 
 
 # In[ ]:
